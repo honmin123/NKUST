0196a42caa3f4874ad36155a863eaab1d5f57e4f
honmin123 <93572258+honmin123@users.noreply.github.com>
2021-11-16 16:16:05 +0800

    Add files via upload

---
+++ bmi.java
@@ -0,0 +1,9 @@
+public class bmi {
+    public static void main(String[] args) {
+        float h = 1.77f;
+        int w = 82;
+        float Bmi = w / (h * h);
+        System.out.println(Bmi);
+    }
+}
+
---
+++ comment.java
@@ -0,0 +1,8 @@
+
+public class comment {
+    public static void main(String[] args) {
+            // This is commemt
+            // Hi hi hi
+            // 這是註解
+    }
+}
