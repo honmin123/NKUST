c4490c475850159a5c71b527a7348ba323a26a95
honmin123 <93572258+honmin123@users.noreply.github.com>
2021-11-23 16:21:55 +0800

    Add files via upload

---
+++ a.java
@@ -0,0 +1,24 @@
+public class a {
+
+
+    public static void main (String[] args) {
+   //     System.out.println(revHash(6933552791181934L));
+        System.out.println(hash("justdoit")); //574318821802
+    }
+
+    public static String letters = "cdefghijlmnoqstuvxz";
+
+   // public static String revHash(long hash) {
+        // write the code here
+   // }
+
+    public static long hash(String s){
+        long h = 7;
+        for (int i = 0; i < s.length(); i++){
+            h = h * 23 + letters.indexOf(s.charAt(i));
+        }
+        return h;
+    }
+}
+
+
